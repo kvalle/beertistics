015e1563063df51e889ecd9e4b3b11939a98a04a
Kjetil Valle <kjetil.valle@gmail.com>
2013-04-10 20:22:12 +0200

    config comment

--- beertistics/config.py
+++ beertistics/config.py
@@ -3,7 +3,7 @@ import os
 # Location of applicatoin root directory
 BASE_DIR = os.path.normpath(os.path.dirname(__file__)+'/../')
 
-# How long to cache data fetched from Untappd
+# How long to cache data fetched from Untappd (in seconds)
 CACHE_TIMEOUT = 1800
 
 # Import all environment specific settings
