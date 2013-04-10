a8e83ef207b7010854491aad5a4dfe1d2c93d2bf
Kjetil Valle <kjetil.valle@gmail.com>
2013-04-10 23:11:48 +0200

    deploy script

--- beertistics/config.py
+++ beertistics/config.py
@@ -1,4 +1,4 @@
-import os
+import sys, os
 
 # Location of applicatoin root directory
 BASE_DIR = os.path.normpath(os.path.dirname(__file__)+'/../')
@@ -6,5 +6,6 @@ BASE_DIR = os.path.normpath(os.path.dirname(__file__)+'/../')
 # How long to cache data fetched from Untappd (in seconds)
 CACHE_TIMEOUT = 1800
 
-# Import all environment specific settings
-from beertistics.env import *
+# Add system and environment specific configuration
+sys.path.append(BASE_DIR + '/conf/')
+from system_env import *
---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e 
+
+version_name=$(date +%F.%H.%M.%S)
+server="kjetil@kjetilvalle.com"
+target="/home/kjetil/web/beertistics"
+
+function info {
+    echo -e "\n\033[32m$1\033[0m"
+}
+
+info "STAGING"
+ssh ${server} "mkdir ${target}/${version_name}"
+scp -r beertistics/* ${server}":"${target}"/"${version_name}"/"
+
+info "UPDATING DEPENDENCIES"
+scp -r requirements.txt ${server}":"${target}/
+ssh ${server} "workon beertistics && cd ${target} && pip install -q -r requirements.txt"
+
+info "RESTARTING"
+scp -r beertistics.wsgi ${server}":"${target}/
+ssh ${server} "cd ${target} && unlink beertistics && ln -s ${version_name} beertistics"
+ssh -t ${server} "sudo service apache2 reload"
