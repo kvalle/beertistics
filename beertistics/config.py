78e62e537fc50a7cc2e9497c153929d74c385bd9
Kjetil Valle <kjetil.valle@gmail.com>
2013-04-10 20:11:12 +0200

    moved some conf parameters into separate environment specific file

--- beertistics/config.py
+++ beertistics/config.py
@@ -1,13 +1,10 @@
-from os.path import dirname, normpath
+import os
 
-SECRET_KEY = 'dev_key'
-DEBUG = True
-BASE_DIR = normpath(dirname(__file__)+'/../')
+# Location of applicatoin root directory
+BASE_DIR = os.path.normpath(os.path.dirname(__file__)+'/../')
 
-## Must be changed according to where app is deployed
-SERVER_NAME = '127.0.0.1:5000'
-
+# How long to cache data fetched from Untappd
 CACHE_TIMEOUT = 1800
 
-UNTAPPD_CLIENT_ID = "C2B98311257562CC0BA3505452861DD8DF65DCC6"
-UNTAPPD_CLIENT_SECRET = "9180BCBA0DFC55C76E125673F4D9F7D9E4A8B324"
+# Import all environment specific settings
+from beertistics.env import *
