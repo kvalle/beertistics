f6cf97dd19ce43520ca1f7b659758a6114a274fc
Kjetil Valle <kjetil.valle@gmail.com>
2013-04-14 19:16:43 +0200

    list of plots on overview page

--- beertistics/static/js/script.js
+++ beertistics/static/js/script.js
@@ -1,12 +1,3 @@
-function basic_stats_message(data) {
-    return "Hello there, " + data.name + ". " + 
-        "In your "+ data.days +" days using Untappd, you have checked in " +
-        "a total of " + data.total + " beers, " + data.distinct + " of which " + 
-        "are distinct. That averages to "+data.total_avg+" beers per day " + 
-        "(" + data.distinct_avg + " distinct). You have also posted " + data.photos +
-        " photos, earned "+data.badges+" badges, and made "+data.friends+" friends."
-}
-
 function make_photo_list(data) {
     rows = $.map(data, function(checkin) {
         return '<li><a title="'+checkin.beer+' by '+checkin.brewery+'"href="'+checkin.photo_original+'"><img src="'+checkin.photo+'"/></a></li>'
