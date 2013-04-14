4af78dbdbb3272c0549fec73e0d44e16bf751339
Kjetil Valle <kjetil.valle@gmail.com>
2013-04-14 22:32:02 +0200

    hover-info on photos page

--- beertistics/static/js/script.js
+++
@@ -1,6 +0,0 @@
-function make_photo_list(data) {
-    rows = $.map(data, function(checkin) {
-        return '<li><a title="'+checkin.beer+' by '+checkin.brewery+'"href="'+checkin.photo_original+'"><img src="'+checkin.photo+'"/></a></li>'
-    });
-    return $('<ul/>', { html: rows.join("") });
-}
