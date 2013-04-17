352470e8d6e153905d6717a445964f0131aafb3b
Kjetil Valle <kjetil.valle@gmail.com>
2013-04-17 22:56:51 +0200

    added some logging

--- beertistics/__init__.py
+++ beertistics/__init__.py
@@ -4,6 +4,15 @@ import config
 app = Flask(__name__)
 app.config.from_object(config)
 
+import logging
+from logging.handlers import RotatingFileHandler
+file_handler = RotatingFileHandler(app.config['BASE_DIR']+'/logs/beertistics.log', 'a+', 1 * 1024 * 1024, 10)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+app.logger.setLevel(logging.INFO)
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+app.logger.info('beertistics startup')
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
--- beertistics/views/api.py
+++ beertistics/views/api.py
@@ -4,6 +4,7 @@ from json import dumps
 from beertistics import app, auth, stats, untappd
 
 def json_response(fn, *args):
+    print app.logger.name
     try:
         data = fn(*args)
         json = dumps(data, indent=4)
