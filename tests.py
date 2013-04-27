ae110373fb39498b0dcd4b262b0284559b7767e4
Kjetil Valle <kjetil.valle@gmail.com>
2013-04-27 13:08:09 +0200

    running tests once before setting up watches

--- tests.py
+++ tests.py
@@ -2,6 +2,8 @@
 
 export BEERTISTICS_CONFIG=test
 
+python -m unittest discover -v beertistics
+
 while inotifywait -r -e modify ./beertistics; do
     python -m unittest discover -v beertistics
 done
