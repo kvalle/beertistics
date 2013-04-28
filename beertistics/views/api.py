f1cd5aad30d5c4a07aa083e9a3fae08bd765f008
Kjetil Valle <kjetil.valle@gmail.com>
2013-04-28 17:52:35 +0200

    made /api/* pages fail unless user is logged in

--- beertistics/views/api.py
+++ beertistics/views/api.py
@@ -1,5 +1,6 @@
 import flask
 import traceback
+from functools import wraps
 from json import dumps
 
 from beertistics import app, auth, stats, untappd, user_service
@@ -15,72 +16,80 @@ def json_response(fn, *args):
         msg = e.message if e.message and app.config["DEBUG"] else "An internal application fuckup occured. Sorry."
         return flask.Response(msg, 500, {'content-type': 'text/plain'})
 
+def fail_unless_logged_in(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not auth.is_logged_in():
+            return flask.Response("User not logged in.", 500, {'content-type': 'text/plain'})
+        return f(*args, **kwargs)
+    return decorated
+
 @app.route('/api/test')
-@auth.requires_auth
+@fail_unless_logged_in
 def test():
     return json_response(stats.test)
 
 @app.route('/api/friends/')
-@auth.requires_auth
+@fail_unless_logged_in
 def friends():
     return json_response(user_service.user_friends)
 
 @app.route('/api/influenced-ratings')
-@auth.requires_auth
+@fail_unless_logged_in
 def influenced_ratings():
     return json_response(stats.influenced_ratings)
 
 @app.route('/api/beers-by-country-as-list')
-@auth.requires_auth
+@fail_unless_logged_in
 def beers_by_country_as_list():
     return json_response(stats.beers_by_country_as_list)
 
 @app.route('/api/beers-by-country')
-@auth.requires_auth
+@fail_unless_logged_in
 def beers_by_country():
     return json_response(stats.beers_by_country)
 
 @app.route('/api/map/checkins')
-@auth.requires_auth
+@fail_unless_logged_in
 def map_checkins():
     return json_response(stats.map_checkins)
 
 @app.route('/api/map/breweries')
-@auth.requires_auth
+@fail_unless_logged_in
 def map_breweries():
     return json_response(stats.map_breweries)
 
 @app.route('/api/rating-distribution')
-@auth.requires_auth
+@fail_unless_logged_in
 def rating_distribution():
     return json_response(stats.rating_distribution)
 
 @app.route('/api/photos')
-@auth.requires_auth
+@fail_unless_logged_in
 def stats_photos():
     return json_response(stats.photos)
 
 @app.route('/api/time-of-day')
-@auth.requires_auth
+@fail_unless_logged_in
 def time_of_day():
     return json_response(stats.time_of_day)
 
 @app.route('/api/rating-vs-abv')
-@auth.requires_auth
+@fail_unless_logged_in
 def rating_vs_abv():
     return json_response(stats.rating_vs_abv)
 
 @app.route('/api/checkin-locations')
-@auth.requires_auth
+@fail_unless_logged_in
 def checkin_locations():
     return json_response(stats.checkin_locations)
 
 @app.route('/api/basic')
-@auth.requires_auth
+@fail_unless_logged_in
 def stats_basic():
     return json_response(stats.basic)
 
 @app.route('/api/per-month')
-@auth.requires_auth
+@fail_unless_logged_in
 def stats_per_month():
     return json_response(stats.per_month)
