4b32d73b3ac7f1ec0b5c3082d5018290316bf8fd
Kjetil Valle <kjetil.valle@gmail.com>
2013-05-08 21:20:46 +0200

    importing dev views only with debug active

--- beertistics/views/__init__.py
+++ beertistics/views/__init__.py
@@ -2,4 +2,7 @@ from beertistics.views import general
 from beertistics.views import stats
 from beertistics.views import login
 from beertistics.views import api
-from beertistics.views import dev
+
+from beertistics import app
+if app.config["DEBUG"]:
+    from beertistics.views import dev
