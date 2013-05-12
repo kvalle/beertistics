b27fd7b367d43779d6771c357b751612e351e595
Kjetil Valle <kjetil.valle@gmail.com>
2013-05-12 20:04:18 +0200

    pre-commit hook

---
+++ scripts/pre-commit.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Script intended for running as git pre-commit hook.
+# Setup hook using
+# 
+#   ln -s scripts/pre-commit.sh .git/hooks/pre-commit
+# 
+# from the projects root directory
+
+# Only verify staged code
+git stash -q --keep-index
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
+
+# Remove all .pyc files in project
+find beertistics | grep ".pyc$" | xargs rm
+
+# Run tests
+export BEERTISTICS_CONFIG=test
+python -m unittest discover -v beertistics
+CODE=$?
+
+if [[ $CODE -ne 0 ]]; then
+    echo -e "\n\033[31mAborting commit: there are failing tests\033[0m"
+fi
+
+# Put back un-staged code
+git stash pop -q
+
+# exit with status code from tests
+exit $CODE
