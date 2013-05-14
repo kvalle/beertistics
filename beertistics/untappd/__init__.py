65adbcec194adc9c5df71f26e2ba6d55b6e8d0f5
Kjetil Valle <kjetil.valle@gmail.com>
2013-05-14 22:27:26 +0200

    renamed untappd module files

--- beertistics/untappd/__init__.py
+++ beertistics/untappd/__init__.py
@@ -3,6 +3,6 @@ from beertistics import app
 DATE_FORMAT = "%a, %d %b %Y %H:%M:%S +0000"
 
 if app.config["UNTAPPD_STUB"]:
-    from beertistics.untappd.untappd_stub import *
+    from beertistics.untappd.stub import *
 else:
-    from beertistics.untappd.untappd_impl import *
+    from beertistics.untappd.impl import *
