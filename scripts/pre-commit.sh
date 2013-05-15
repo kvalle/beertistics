54305d16d58ff34c005a7cee8338a6b318c9d77e
Kjetil Valle <kjetil.valle@gmail.com>
2013-05-15 20:49:41 +0200

    added running of flake8 to pre-commit hook

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -7,21 +7,37 @@
 # 
 # from the projects root directory
 
+function fail {
+    echo -e "\033[31m->" $1 "\033[0m"
+}
+
+function info {
+    echo -e "\033[32m-->" $1 "\033[0m"
+}
+
 # Only verify staged code
 git stash -q --keep-index
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
 
+
 # Remove all .pyc files in project
+info "removing .pyc files"
 find beertistics | grep ".pyc$" | xargs rm
 
 # Run tests
+info "running tests"
 export BEERTISTICS_CONFIG=test
 python -m unittest discover --failfast -s beertistics/tests/ -p '*.py'
 CODE=$?
 
-if [[ $CODE -ne 0 ]]; then
-    echo -e "\n\033[31mAborting commit: there are failing tests\033[0m"
+if [[ $CODE -ne 0 ]] 
+then
+    fail "aborting commit: there are failing tests"
+else
+    # Run flake8 to check for code problems
+    info "checking for code smells"
+    flake8 beertistics
 fi
 
 # Put back un-staged code
