6fb63c036ab970b9256ac6b11971f92b4882f3a6
Kjetil Valle <kjetil.valle@gmail.com>
2013-05-25 13:49:31 +0200

    formatting

--- beertistics/search/stub.py
+++ beertistics/search/stub.py
@@ -2,30 +2,23 @@ import json
 
 DATE_FORMAT = "%a, %d %b %Y %H:%M:%S +0000"
 
-
 def update_last_indexed(username, datatype):
     pass
-
 
 def get_last_indexed(username, datatype):
     return None
 
-
 def is_current(username, datatype):
     return True
-
 
 def index(datatype, items):
     pass
 
-
 def search(datatype, query={}):
     return _default_result(datatype)
 
-
 def get(datatype, id):
     return _default_result(datatype)
-
 
 def _default_result(datatype):
     with open("stub/valle/%s.json" % datatype) as f:
