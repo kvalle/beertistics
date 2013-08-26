995d1bbd8cc252d303b2f096b6d1f56ed150fb2a
Kjetil Valle <kjetil.valle@gmail.com>
2013-08-27 00:47:57 +0200

    './scripts/manage.py runserver' is the new way to start local dev server

---
+++ scripts/manage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+import os.path
+import json
+
+# Add project root to python path, so we'll find beertistics
+current_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(current_dir + "/..")
+
+from beertistics import app
+from flask.ext.script import Manager
+from manage_es import manager as es
+
+manager = Manager(app)
+
+@manager.command
+def hello():
+    """Hello world, manager style"""
+    print "Hello, got those TPS reports ready?"
+
+if __name__ == "__main__":
+    manager.add_command("es", es)
+    manager.run()
