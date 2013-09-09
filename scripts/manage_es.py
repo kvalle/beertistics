5c0a119ff61aa30597f952d58d70db06243c1e2f
Kjetil Valle <kjetil.valle@gmail.com>
2013-09-09 23:14:09 +0200

    made sure it's not possible to start another ES while first is booting

--- scripts/manage_es.py
+++ scripts/manage_es.py
@@ -24,6 +24,9 @@ def es_is_running():
     except SocketError, e:
         return False
 
+def es_pidfile_exists():
+    return isfile("es.pid")
+
 def ensure_es_running(fn):
     """Wrapper for catching ES connection exception"""
     @wraps(fn)
@@ -39,8 +42,8 @@ def ensure_es_running(fn):
 #
 
 manager = Manager(app, \
-    with_default_commands=False, \
-    usage="Perform operations on Elastic Search")
+            with_default_commands=False, \
+            usage="Perform operations on Elastic Search")
 
 @manager.command
 @ensure_es_running
@@ -62,19 +65,22 @@ def start():
     """Start an ES server instance"""
     if es_is_running():
         return "ES is already running..."
+    if es_pidfile_exists():
+        msg = "PID-file for ES exist. "
+        msg += "If you are sure ES is not running, remove file 'es.pid'"
+        return msg
     start_es = "es/bin/elasticsearch -p es.pid"
     subprocess.Popen(start_es, shell=True)
-    while not isfile("es.pid"):
+    while not es_pidfile_exists():
         time.sleep(0.1)
     pid = subprocess.check_output(["cat", "es.pid"])
     return "ES is starting with PID %s" % pid
 
 @manager.command
+@ensure_es_running
 def stop():
     """Stop the currently running ES instance"""
-    if not es_is_running():
-        return "ES don't appear to be running..."
-    if not isfile("es.pid"):
+    if not es_pidfile_exists():
         return "ES is running, but I don't know the PID"
     pid = subprocess.check_output(["cat", "es.pid"])
     return_code = subprocess.call(["kill", pid])
